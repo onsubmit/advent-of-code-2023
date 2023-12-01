754ffd6d2ba3d2bf1ad14aa790312dcdd2e76cd0
Andy <onsubmit@outlook.com>
2023-11-30 21:42:55 -0800

    Add day 1 part 1

--- src/days/day01.ts
+++ src/days/day01.ts
@@ -1,8 +1,16 @@
+import FileReader from '../fileReader';
+import { sumArray } from '../sumArray';
 import Day from './day';
 
 export default class Day01 extends Day {
   getPartOneSolution = (): string => {
-    return '0';
+    const numbers = FileReader.getLines('../input/day01-1.txt', (line) => {
+      const first = line.match(/\d/)?.at(0);
+      const last = [...line].reverse().join().match(/\d/)?.at(0);
+      return parseInt(`${first}${last}`, 10);
+    });
+
+    return sumArray(numbers).toString();
   };
 
   getPartTwoSolution = (): string => {
