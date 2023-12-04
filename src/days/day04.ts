5a76a964badfbbf9af2eea82db6f1051572bd726
Andy <onsubmit@outlook.com>
2023-12-04 11:32:52 -0800

    Add day 4 part 2

--- src/days/day04.ts
+++ src/days/day04.ts
@@ -2,6 +2,7 @@ import { sumArray } from '../sumArray';
 
 type Card = {
   number: number;
+  copies: number;
   winningNumbers: Set<number>;
   numbersOnCard: number[];
   score: number;
@@ -32,6 +33,7 @@ export const getPartOneSolution = (input: string): string => {
 
     return {
       number: cardNumber,
+      copies: 1,
       winningNumbers,
       numbersOnCard,
       score: numWinningNumbersOnCard === 0 ? 0 : Math.pow(2, numWinningNumbersOnCard - 1),
@@ -44,5 +46,46 @@ export const getPartOneSolution = (input: string): string => {
 export const getPartTwoSolution = (input: string): string => {
   const lines = input.split('\n').filter(Boolean);
 
-  return lines.join();
+  const numExtraCopies: Map<number, number> = new Map();
+
+  const cards = lines.map<Card>((line, i) => {
+    const cardNumber = i + 1;
+    const split = line.split(':')[1].split('|');
+
+    const winningNumbers = new Set(
+      split[0]
+        .trim()
+        .split(' ')
+        .filter(Boolean)
+        .map((s) => parseInt(s, 10))
+    );
+
+    const numbersOnCard = split[1]
+      .trim()
+      .split(' ')
+      .filter(Boolean)
+      .map((s) => parseInt(s, 10));
+
+    const numWinningNumbersOnCard = numbersOnCard.filter((num) => winningNumbers.has(num)).length;
+    for (let i = 0; i <= (numExtraCopies.get(cardNumber) ?? 0); i++) {
+      for (let j = 1; j <= numWinningNumbersOnCard; j++) {
+        if (numExtraCopies.has(cardNumber + j)) {
+          const value = numExtraCopies.get(cardNumber + j)!;
+          numExtraCopies.set(cardNumber + j, value + 1);
+        } else {
+          numExtraCopies.set(cardNumber + j, 1);
+        }
+      }
+    }
+
+    return {
+      number: cardNumber,
+      copies: 1 + (numExtraCopies.get(cardNumber) ?? 0),
+      winningNumbers,
+      numbersOnCard,
+      score: numWinningNumbersOnCard === 0 ? 0 : Math.pow(2, numWinningNumbersOnCard - 1),
+    };
+  });
+
+  return sumArray(cards.map((c) => c.copies)).toString();
 };
