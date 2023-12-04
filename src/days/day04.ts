de681a55e1ca5a1d0f2e49910f9c7fa808f3758c
Andy <onsubmit@outlook.com>
2023-12-04 11:36:46 -0800

    Consolidate solution

--- src/days/day04.ts
+++ src/days/day04.ts
@@ -8,47 +8,10 @@ type Card = {
   score: number;
 };
 
-export const getPartOneSolution = (input: string): string => {
-  const lines = input.split('\n').filter(Boolean);
-
-  const cards = lines.map<Card>((line, i) => {
-    const cardNumber = i + 1;
-    const split = line.split(':')[1].split('|');
-
-    const winningNumbers = new Set(
-      split[0]
-        .trim()
-        .split(' ')
-        .filter(Boolean)
-        .map((s) => parseInt(s, 10))
-    );
-
-    const numbersOnCard = split[1]
-      .trim()
-      .split(' ')
-      .filter(Boolean)
-      .map((s) => parseInt(s, 10));
-
-    const numWinningNumbersOnCard = numbersOnCard.filter((num) => winningNumbers.has(num)).length;
-
-    return {
-      number: cardNumber,
-      copies: 1,
-      winningNumbers,
-      numbersOnCard,
-      score: numWinningNumbersOnCard === 0 ? 0 : Math.pow(2, numWinningNumbersOnCard - 1),
-    };
-  });
-
-  return sumArray(cards.map((c) => c.score)).toString();
-};
-
-export const getPartTwoSolution = (input: string): string => {
-  const lines = input.split('\n').filter(Boolean);
-
+const getCards = (lines: string[]): Card[] => {
   const numExtraCopies: Map<number, number> = new Map();
 
-  const cards = lines.map<Card>((line, i) => {
+  return lines.map<Card>((line, i) => {
     const cardNumber = i + 1;
     const split = line.split(':')[1].split('|');
 
@@ -86,6 +49,18 @@ export const getPartTwoSolution = (input: string): string => {
       score: numWinningNumbersOnCard === 0 ? 0 : Math.pow(2, numWinningNumbersOnCard - 1),
     };
   });
+};
+
+export const getPartOneSolution = (input: string): string => {
+  const lines = input.split('\n').filter(Boolean);
+  const cards = getCards(lines);
+
+  return sumArray(cards.map((c) => c.score)).toString();
+};
+
+export const getPartTwoSolution = (input: string): string => {
+  const lines = input.split('\n').filter(Boolean);
+  const cards = getCards(lines);
 
   return sumArray(cards.map((c) => c.copies)).toString();
 };
