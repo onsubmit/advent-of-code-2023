f911c3fcee642b6c5409bb9c8e3884e9869660b4
Andy <onsubmit@outlook.com>
2023-12-05 18:38:24 -0800

    Add brute force solution for day 5 part 2

--- src/days/day05.ts
+++ src/days/day05.ts
@@ -4,6 +4,11 @@ type RangeData = {
   length: number;
 };
 
+type SeedInterval = {
+  start: number;
+  end: number;
+};
+
 const mapTypes = [
   'seed-to-soil',
   'soil-to-fertilizer',
@@ -54,23 +59,76 @@ export const getPartOneSolution = (input: string): string => {
 
   let minLocationNumber = Number.MAX_SAFE_INTEGER;
   for (const seed of seeds) {
-    let mappedVal = seed;
-    for (const map of maps) {
-      const data = map.find(
-        (x) => mappedVal >= x.sourceStart && mappedVal <= x.sourceStart + x.length
-      );
-
-      mappedVal = data ? data.destinationStart + (mappedVal - data.sourceStart) : mappedVal;
-    }
-
-    minLocationNumber = Math.min(minLocationNumber, mappedVal);
+    const location = getLocationForSeed(seed, maps);
+    minLocationNumber = Math.min(minLocationNumber, location);
   }
 
   return minLocationNumber.toString();
 };
 
 export const getPartTwoSolution = (input: string): string => {
-  const lines = input.split('\n').filter(Boolean);
+  const lines = input.split('\n');
 
-  return lines.join();
+  const seedIntervals: SeedInterval[] = [];
+  const maps: Array<Array<RangeData>> = [];
+
+  for (const line of lines) {
+    if (!line.trim()) {
+      maps.push([]);
+      continue;
+    }
+
+    if (line.startsWith('seeds:')) {
+      const mapped = line
+        .split(':')[1]
+        .trim()
+        .split(' ')
+        .filter(Boolean)
+        .map((d) => parseInt(d, 10));
+
+      for (let i = 0; i < mapped.length; i += 2) {
+        seedIntervals.push({ start: mapped[i], end: mapped[i] + mapped[i + 1] - 1 });
+      }
+
+      continue;
+    }
+
+    if (mapTypes.some((mapType) => line.startsWith(mapType))) {
+      continue;
+    }
+
+    const [destinationRangeStart, sourceRangeStart, rangeLength] = line
+      .split(' ')
+      .filter(Boolean)
+      .map((d) => parseInt(d, 10));
+
+    maps.at(-1)?.push({
+      destinationStart: destinationRangeStart,
+      sourceStart: sourceRangeStart,
+      length: rangeLength,
+    });
+  }
+
+  let minLocationNumber = Number.MAX_SAFE_INTEGER;
+  for (const seedInterval of seedIntervals) {
+    for (let seed = seedInterval.start; seed <= seedInterval.end; seed++) {
+      const location = getLocationForSeed(seed, maps);
+      minLocationNumber = Math.min(minLocationNumber, location);
+    }
+  }
+
+  return minLocationNumber.toString();
+};
+
+const getLocationForSeed = (seed: number, maps: Array<Array<RangeData>>): number => {
+  let mappedVal = seed;
+  for (const map of maps) {
+    const data = map.find(
+      (x) => mappedVal >= x.sourceStart && mappedVal < x.sourceStart + x.length
+    );
+
+    mappedVal = data ? data.destinationStart + (mappedVal - data.sourceStart) : mappedVal;
+  }
+
+  return mappedVal;
 };
