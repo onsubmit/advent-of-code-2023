738408156d6ea20ecea91ddf72174dd4dba40560
Andy <onsubmit@outlook.com>
2023-12-05 19:34:51 -0800

    Add day 6 boilerplate

---
+++ src/components/day06.tsx
@@ -0,0 +1,7 @@
+import { getPartOneSolution, getPartTwoSolution } from '../days/day06';
+import Day from './day';
+
+export default function Day06() {
+  const props = { day: 6, getPartOneSolution, getPartTwoSolution };
+  return <Day {...props} />;
+}
