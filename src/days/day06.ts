8464f16d6719c5ffbb1e0f72de49735f646ddd0b
Andy <onsubmit@outlook.com>
2023-12-05 21:14:07 -0800

    Add day 6 part 1

--- src/days/day06.ts
+++ src/days/day06.ts
@@ -1,7 +1,42 @@
+import { multiplyArray } from '../sumArray';
+
+type Race = {
+  time: number;
+  currentDistanceRecord: number;
+};
+
 export const getPartOneSolution = (input: string): string => {
   const lines = input.split('\n');
 
-  return lines.join();
+  const times = lines[0]
+    .split(':')[1]
+    .trim()
+    .split(' ')
+    .filter(Boolean)
+    .map((d) => parseInt(d, 10));
+
+  const distances = lines[1]
+    .split(':')[1]
+    .trim()
+    .split(' ')
+    .filter(Boolean)
+    .map((d) => parseInt(d, 10));
+
+  const races: Race[] = times.map((_, i) => ({
+    time: times[i],
+    currentDistanceRecord: distances[i],
+  }));
+
+  const numWaysToWin: number[] = [];
+
+  for (const race of races) {
+    const winners = Array.from({ length: race.time }, (_, i) => i).filter(
+      (s) => (race.time - s) * s > race.currentDistanceRecord
+    );
+    numWaysToWin.push(winners.length);
+  }
+
+  return multiplyArray(numWaysToWin).toString();
 };
 
 export const getPartTwoSolution = (input: string): string => {
--- src/sumArray.ts
+++ src/sumArray.ts
@@ -1,1 +1,3 @@
 export const sumArray = (arr: number[]) => arr.reduce((acc, a) => acc + a, 0);
+
+export const multiplyArray = (arr: number[]) => arr.reduce((acc, a) => acc * a, 1);
