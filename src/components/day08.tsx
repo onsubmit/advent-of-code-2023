5ab206a05895dd0930745c844a469503b861937b
Andy <onsubmit@outlook.com>
2023-12-07 18:57:44 -0800

    Add day 8 boilerplate

---
+++ src/components/day08.tsx
@@ -0,0 +1,7 @@
+import { getPartOneSolution, getPartTwoSolution } from '../days/day08';
+import Day from './day';
+
+export default function Day08() {
+  const props = { day: 8, getPartOneSolution, getPartTwoSolution };
+  return <Day {...props} />;
+}
