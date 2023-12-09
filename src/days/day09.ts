82386aac61c80aeae14fad7d8a502555bf511b93
Andy <onsubmit@outlook.com>
2023-12-08 21:19:11 -0800

    Add day 9 part 1

--- src/days/day09.ts
+++ src/days/day09.ts
@@ -1,7 +1,41 @@
 export const getPartOneSolution = (input: string): string => {
   const lines = input.split('\n').filter(Boolean);
 
-  return lines.toString();
+  const numberSets: number[][] = lines.map((line) =>
+    line
+      .trim()
+      .split(' ')
+      .map((d) => parseInt(d.trim(), 10))
+  );
+
+  let sum = 0;
+  for (const numberSet of numberSets) {
+    const differences: number[][] = [[...numberSet]];
+    let go = true;
+    while (go) {
+      const nextRow: number[] = [];
+      const currentRow = differences.at(-1)!;
+      for (let i = 0; i < currentRow.length - 1; i++) {
+        nextRow.push(currentRow[i + 1] - currentRow[i]);
+      }
+      differences.push(nextRow);
+      if (nextRow.every((d) => d === 0)) {
+        go = false;
+      }
+    }
+
+    differences.at(-1)?.push(0);
+    const endNeg2 = differences.at(-2)?.at(-1) ?? 0;
+    differences.at(-2)?.push(endNeg2);
+    for (let i = 3; i <= differences.length; i++) {
+      const endThisRow = differences.at(0 - i)?.at(-1) ?? 0;
+      const endBelowRow = differences.at(1 - i)?.at(-1) ?? 0;
+      differences.at(0 - i)?.push(endThisRow + endBelowRow);
+    }
+    sum += differences.at(0)?.at(-1) ?? 0;
+  }
+
+  return sum.toString();
 };
 
 export const getPartTwoSolution = (input: string): string => {
