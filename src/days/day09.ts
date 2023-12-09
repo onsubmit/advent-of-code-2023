26a92a077c10bb95553b18eb61e8b189e2ccc250
Andy <onsubmit@outlook.com>
2023-12-08 22:11:27 -0800

    Clean up day 9

--- src/days/day09.ts
+++ src/days/day09.ts
@@ -1,78 +1,70 @@
-export const getPartOneSolution = (input: string): string => {
+const getValueHistories = (input: string): number[][] => {
   const lines = input.split('\n').filter(Boolean);
 
-  const numberSets: number[][] = lines.map((line) =>
+  return lines.map((line) =>
     line
       .trim()
       .split(' ')
       .map((d) => parseInt(d.trim(), 10))
   );
+};
+
+const getDifferences = (valueHistory: number[]): number[][] => {
+  const differences: number[][] = [[...valueHistory]];
+  while (differences.at(-1)?.some((d) => d !== 0)) {
+    const nextRow: number[] = [];
+    const currentRow = differences.at(-1)!;
+    for (let i = 0; i < currentRow.length - 1; i++) {
+      nextRow.push(currentRow[i + 1] - currentRow[i]);
+    }
+    differences.push(nextRow);
+  }
+
+  return differences;
+};
+
+export const getPartOneSolution = (input: string): string => {
+  const valueHistories = getValueHistories(input);
 
   let sum = 0;
-  for (const numberSet of numberSets) {
-    const differences: number[][] = [[...numberSet]];
-    let go = true;
-    while (go) {
-      const nextRow: number[] = [];
-      const currentRow = differences.at(-1)!;
-      for (let i = 0; i < currentRow.length - 1; i++) {
-        nextRow.push(currentRow[i + 1] - currentRow[i]);
-      }
-      differences.push(nextRow);
-      if (nextRow.every((d) => d === 0)) {
-        go = false;
-      }
+  for (const valueHistory of valueHistories) {
+    const differences = getDifferences(valueHistory);
+
+    // Last row has all 0s, push another.
+    differences.at(-1)?.push(0);
+
+    // Value to push to the remaining rows is the sum of the
+    // last items from its row and the one below it.
+    for (let i = 2; i <= differences.length; i++) {
+      const lastThisRow = differences.at(0 - i)!.at(-1)!;
+      const lastBelowRow = differences.at(1 - i)!.at(-1)!;
+      differences.at(0 - i)?.push(lastThisRow + lastBelowRow);
     }
 
-    differences.at(-1)?.push(0);
-    const endNeg2 = differences.at(-2)?.at(-1) ?? 0;
-    differences.at(-2)?.push(endNeg2);
-    for (let i = 3; i <= differences.length; i++) {
-      const endThisRow = differences.at(0 - i)?.at(-1) ?? 0;
-      const endBelowRow = differences.at(1 - i)?.at(-1) ?? 0;
-      differences.at(0 - i)?.push(endThisRow + endBelowRow);
-    }
-    sum += differences.at(0)?.at(-1) ?? 0;
+    sum += differences.at(0)!.at(-1)!;
   }
 
   return sum.toString();
 };
 
 export const getPartTwoSolution = (input: string): string => {
-  const lines = input.split('\n').filter(Boolean);
-
-  const numberSets: number[][] = lines.map((line) =>
-    line
-      .trim()
-      .split(' ')
-      .map((d) => parseInt(d.trim(), 10))
-  );
+  const valueHistories = getValueHistories(input);
 
   let sum = 0;
-  for (const numberSet of numberSets) {
-    const differences: number[][] = [[...numberSet]];
-    let go = true;
-    while (go) {
-      const nextRow: number[] = [];
-      const currentRow = differences.at(-1)!;
-      for (let i = 0; i < currentRow.length - 1; i++) {
-        nextRow.push(currentRow[i + 1] - currentRow[i]);
-      }
-      differences.push(nextRow);
-      if (nextRow.every((d) => d === 0)) {
-        go = false;
-      }
-    }
+  for (const valueHistory of valueHistories) {
+    const differences = getDifferences(valueHistory);
 
+    // Last row has all 0s, insert  another.
     differences.at(-1)?.unshift(0);
-    const begNeg2 = differences.at(-2)?.at(0) ?? 0;
-    differences.at(-2)?.unshift(begNeg2);
-    for (let i = 3; i <= differences.length; i++) {
-      const begThisRow = differences.at(0 - i)?.at(0) ?? 0;
-      const begBelowRow = differences.at(1 - i)?.at(0) ?? 0;
-      differences.at(0 - i)?.unshift(begThisRow - begBelowRow);
+
+    // Value to insert to the remaining rows is the difference between the
+    // last items from its row and the one below it.
+    for (let i = 2; i <= differences.length; i++) {
+      const startThisRow = differences.at(0 - i)!.at(0)!;
+      const startBelowRow = differences.at(1 - i)!.at(0)!;
+      differences.at(0 - i)?.unshift(startThisRow - startBelowRow);
     }
-    sum += differences.at(0)?.at(0) ?? 0;
+    sum += differences.at(0)!.at(0)!;
   }
 
   return sum.toString();
