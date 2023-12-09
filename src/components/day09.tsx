c3b7a35e23ab4fc79535ad8aa5dfa50ad3deca03
Andy <onsubmit@outlook.com>
2023-12-08 20:45:10 -0800

    Add day 9 boilerplate

---
+++ src/components/day09.tsx
@@ -0,0 +1,7 @@
+import { getPartOneSolution, getPartTwoSolution } from '../days/day09';
+import Day from './day';
+
+export default function Day09() {
+  const props = { day: 9, getPartOneSolution, getPartTwoSolution };
+  return <Day {...props} />;
+}
---
+++ src/days/day09.ts
@@ -0,0 +1,11 @@
+export const getPartOneSolution = (input: string): string => {
+  const lines = input.split('\n').filter(Boolean);
+
+  return lines.toString();
+};
+
+export const getPartTwoSolution = (input: string): string => {
+  const lines = input.split('\n').filter(Boolean);
+
+  return lines.toString();
+};
