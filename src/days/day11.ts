f47ae419bf3185363759ce198d05929cf8d1471c
Andy <onsubmit@outlook.com>
2023-12-10 21:38:56 -0800

    Add day 11 part 2

--- src/days/day11.ts
+++ src/days/day11.ts
@@ -87,6 +87,69 @@ const getShortestPathLengthsOfStarPairs = (starCoordinates: Coordinate[]): numbe
   return pathLengths;
 };
 
+const getShortestPathLengthsOfStarPairsPart2 = (
+  universe: string[][],
+  starCoordinates: Coordinate[]
+): number[] => {
+  const emptyRows: number[] = [];
+  const emptyCols: number[] = [];
+
+  for (let r = 0; r < universe.length; r++) {
+    if (universe[r].every((x) => x === '.')) {
+      emptyRows.push(r);
+    }
+  }
+
+  for (let c = 0; c < universe[0].length; c++) {
+    let isColumnEmpty = true;
+    for (let r = 0; isColumnEmpty && r < universe.length; r++) {
+      if (universe[r][c] !== '.') {
+        isColumnEmpty = false;
+      }
+    }
+
+    if (isColumnEmpty) {
+      emptyCols.push(c);
+    }
+  }
+
+  const pathLengths: number[] = [];
+  const known = new Map<number, Map<number, number>>();
+  for (let i = 0; i < starCoordinates.length; i++) {
+    for (let j = 0; j < starCoordinates.length; j++) {
+      if (i === j) {
+        continue;
+      }
+
+      if (known.get(i)?.get(j) !== undefined) {
+        continue;
+      }
+
+      if (!known.get(i)) {
+        known.set(i, new Map());
+      }
+
+      const minRow = Math.min(starCoordinates[i].row, starCoordinates[j].row);
+      const minCol = Math.min(starCoordinates[i].column, starCoordinates[j].column);
+      const maxRow = Math.max(starCoordinates[i].row, starCoordinates[j].row);
+      const maxCol = Math.max(starCoordinates[i].column, starCoordinates[j].column);
+      const numEmptyRowsBetweenStars = emptyRows.filter((n) => n > minRow && n < maxRow).length;
+      const numEmptyColsBetweenStars = emptyCols.filter((n) => n > minCol && n < maxCol).length;
+
+      const multiplier = 1000000;
+      const distance =
+        (multiplier - 1) * (numEmptyRowsBetweenStars + numEmptyColsBetweenStars) +
+        Math.abs(starCoordinates[i].row - starCoordinates[j].row) +
+        Math.abs(starCoordinates[i].column - starCoordinates[j].column);
+
+      known.get(i)?.set(j, distance);
+      pathLengths.push(distance);
+    }
+  }
+
+  return pathLengths;
+};
+
 export const getPartOneSolution = (input: string): string => {
   const lines = input.split('\n').filter(Boolean);
 
@@ -101,5 +164,12 @@ export const getPartOneSolution = (input: string): string => {
 export const getPartTwoSolution = (input: string): string => {
   const lines = input.split('\n').filter(Boolean);
 
-  return lines.toString();
+  const universe: string[][] = lines.map((line) => [...line]);
+  const starCoordinates: Coordinate[] = getStarCoordinates(universe);
+  const shortestPathLengthsOfStarPairs = getShortestPathLengthsOfStarPairsPart2(
+    universe,
+    starCoordinates
+  );
+
+  return (sumArray(shortestPathLengthsOfStarPairs) / 2).toString();
 };
