f17619494d7d81235a47f1358dbc200c4a7bc68e
Andy <onsubmit@outlook.com>
2023-12-11 23:20:36 -0800

    Add day 12 part 1

--- src/days/day12.ts
+++ src/days/day12.ts
@@ -1,7 +1,94 @@
+import Iter from 'es-iter';
+
+import { sumArray } from '../arrayMethods';
+
+type SpringStatus = '.' | '#' | '?';
+type DamagedGroup = {
+  size: number;
+  numUnknown: number;
+};
+
+type Record = {
+  springs: SpringStatus[];
+  springGroups: DamagedGroup[];
+  damagedSpringGroupsLengths: number[];
+};
+
 export const getPartOneSolution = (input: string): string => {
   const lines = input.split('\n').filter(Boolean);
 
-  return lines.toString();
+  const records = lines.map<Record>((line) => {
+    const split = line.split(' ');
+    const springs = [...split[0]] as SpringStatus[];
+    const springGroups = split[0]
+      .split('.')
+      .filter(Boolean)
+      .map<DamagedGroup>((g) => ({
+        size: g.length,
+        numUnknown: [...g].filter((c) => c === '?').length,
+      }));
+    const damagedSpringGroupsLengths = split[1].split(',').map((d) => parseInt(d, 10));
+    return {
+      springs,
+      springGroups,
+      damagedSpringGroupsLengths,
+    };
+  });
+
+  const recordNumArrangements: number[] = [];
+
+  for (const record of records) {
+    const width = record.springs.length;
+    const totalOptions =
+      sumArray(record.damagedSpringGroupsLengths) + record.damagedSpringGroupsLengths.length - 1;
+    const extraSquares = width - totalOptions;
+    const range = Array.from(
+      { length: record.damagedSpringGroupsLengths.length + extraSquares },
+      (_, i) => i
+    );
+
+    const combinations: number[][] = [
+      ...new Iter(range).combinations(record.damagedSpringGroupsLengths.length),
+    ];
+
+    const rows = combinations
+      .map((combination) => {
+        const row: SpringStatus[] = [];
+        for (let i = 0; i < combination.length; i++) {
+          row.push(
+            ...Array.from(
+              { length: combination[i] - (combination[i - 1] ?? 0) },
+              () => '.' as SpringStatus
+            )
+          );
+          row.push(
+            ...Array.from(
+              { length: record.damagedSpringGroupsLengths[i] },
+              () => '#' as SpringStatus
+            )
+          );
+        }
+
+        row.push(...Array.from({ length: width - row.length }, () => '.' as SpringStatus));
+
+        for (let j = 0; j < width; j++) {
+          if (record.springs[j] === '?') {
+            continue;
+          }
+
+          if (row[j] !== record.springs[j]) {
+            return;
+          }
+        }
+
+        return row;
+      })
+      .filter(Boolean);
+
+    recordNumArrangements.push(rows.length);
+  }
+
+  return sumArray(recordNumArrangements).toString();
 };
 
 export const getPartTwoSolution = (input: string): string => {
