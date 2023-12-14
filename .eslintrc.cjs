c2575c2992706ba8d9d1f0337b39ac3e092188b1
Andy <onsubmit@outlook.com>
2023-12-14 13:06:43 -0800

    Allow for while(true) loops

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -47,6 +47,7 @@ module.exports = {
     ],
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
+    'no-constant-condition': ['error', { checkLoops: false }],
   },
   overrides: [
     {
