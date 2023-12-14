e7e893c42922651597dcd4d4ccecbee64261d4e5
Andy <onsubmit@outlook.com>
2023-12-13 20:40:08 -0800

    Add day 14 boilerplate

---
+++ src/components/day14.tsx
@@ -0,0 +1,7 @@
+import { getPartOneSolution, getPartTwoSolution } from '../days/day14';
+import Day from './day';
+
+export default function Day14() {
+  const props = { day: 14, getPartOneSolution, getPartTwoSolution };
+  return <Day {...props} />;
+}
