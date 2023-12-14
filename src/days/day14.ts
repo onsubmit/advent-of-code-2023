47ccfbecdafd7b2b1637dd1b33ca29a4bd552b11
Andy <onsubmit@outlook.com>
2023-12-14 13:06:55 -0800

    Refactor day 14

--- src/days/day14.ts
+++ src/days/day14.ts
@@ -1,163 +1,160 @@
+type Space = '.' | '#' | 'O';
+type Direction = 'north' | 'south' | 'east' | 'west';
+
+class Platform {
+  private _spaces: Array<Array<Space>>;
+  private readonly _rows: number;
+  private readonly _columns: number;
+
+  constructor(lines: string[]) {
+    this._spaces = lines.map<Array<Space>>((line) => [...line] as Array<Space>);
+    this._rows = this._spaces.length;
+    this._columns = this._spaces[0].length;
+  }
+
+  tiltLever = (slideDirection: Direction) => {
+    switch (slideDirection) {
+      case 'north':
+        return this._slideNorth();
+      case 'south':
+        return this._slideSouth();
+      case 'east':
+        return this._slideEast();
+      case 'west':
+        return this._slideWest();
+    }
+  };
+
+  cycle = () => {
+    this.tiltLever('north');
+    this.tiltLever('west');
+    this.tiltLever('south');
+    this.tiltLever('east');
+  };
+
+  getTotalLoad = (): number => {
+    let sum = 0;
+    for (let r = 0; r < this._rows; r++) {
+      sum += (this._rows - r) * this._spaces[r].filter((s) => s === 'O').length;
+    }
+
+    return sum;
+  };
+
+  toString = (): string => this._spaces.map((s) => s.join('')).join('\n');
+
+  private _slideNorth = () => {
+    while (true) {
+      let didSlide = false;
+      for (let c = 0; c < this._columns; c++) {
+        for (let r = 1; r < this._rows; r++) {
+          if (this._spaces[r - 1][c] === '.' && this._spaces[r][c] === 'O') {
+            this._spaces[r - 1][c] = 'O';
+            this._spaces[r][c] = '.';
+            didSlide = true;
+          }
+        }
+      }
+
+      if (!didSlide) {
+        return;
+      }
+    }
+  };
+
+  private _slideSouth = () => {
+    while (true) {
+      let didSlide = false;
+      for (let c = 0; c < this._columns; c++) {
+        for (let r = this._rows - 2; r >= 0; r--) {
+          if (this._spaces[r + 1][c] === '.' && this._spaces[r][c] === 'O') {
+            this._spaces[r + 1][c] = 'O';
+            this._spaces[r][c] = '.';
+            didSlide = true;
+          }
+        }
+      }
+
+      if (!didSlide) {
+        return;
+      }
+    }
+  };
+
+  private _slideEast = () => {
+    while (true) {
+      let didSlide = false;
+      for (let r = 0; r < this._rows; r++) {
+        for (let c = this._columns - 2; c >= 0; c--) {
+          if (this._spaces[r][c + 1] === '.' && this._spaces[r][c] === 'O') {
+            this._spaces[r][c + 1] = 'O';
+            this._spaces[r][c] = '.';
+            didSlide = true;
+          }
+        }
+      }
+
+      if (!didSlide) {
+        return;
+      }
+    }
+  };
+
+  private _slideWest = () => {
+    while (true) {
+      let didSlide = false;
+      for (let r = 0; r < this._rows; r++) {
+        for (let c = 1; c < this._columns; c++) {
+          if (this._spaces[r][c - 1] === '.' && this._spaces[r][c] === 'O') {
+            this._spaces[r][c - 1] = 'O';
+            this._spaces[r][c] = '.';
+            didSlide = true;
+          }
+        }
+      }
+
+      if (!didSlide) {
+        return;
+      }
+    }
+  };
+}
+
 export const getPartOneSolution = (input: string): string => {
   const lines = input.split('\n').filter(Boolean);
 
-  const spaces = lines.map((line) => [...line]);
+  const platform = new Platform(lines);
+  platform.tiltLever('north');
 
-  let iterate = true;
-  while (iterate) {
-    let didSlide = false;
-    for (let c = 0; c < lines[0].length; c++) {
-      for (let r = 1; r < lines.length; r++) {
-        if (spaces[r - 1][c] === '.' && spaces[r][c] === 'O') {
-          spaces[r - 1][c] = 'O';
-          spaces[r][c] = '.';
-          didSlide = true;
-        }
-      }
-    }
-
-    if (!didSlide) {
-      iterate = false;
-    }
-  }
-
-  let sum = 0;
-  for (let r = 0; r < lines.length; r++) {
-    sum += (lines.length - r) * spaces[r].filter((s) => s === 'O').length;
-  }
-
-  return sum.toString();
+  return platform.getTotalLoad().toString();
 };
 
 export const getPartTwoSolution = (input: string): string => {
   const lines = input.split('\n').filter(Boolean);
 
-  const spaces = lines.map((line) => [...line]);
+  const getTotalLoad = () => {
+    const platform = new Platform(lines);
+    const platformToCycleMap: Map<string, number> = new Map();
+    const cycleToTotalLoadMap: Map<number, number> = new Map();
 
-  const previous: Map<string, number> = new Map();
-  const known: Map<number, number[]> = new Map();
-  for (let cycle = 0; cycle < 10000; cycle++) {
-    //console.log('cycle', cycle);
-    //console.log('initial');
-    //console.log(spaces.map((s) => s.join('')).join('\n'));
-    const stringRep = spaces.map((s) => s.join('')).join('\n');
-    if (previous.has(stringRep)) {
-      const repeat = previous.get(stringRep);
-      console.log({ repeat, cycle });
-      debugger;
+    const maxCycle = 1_000_000_000;
+    for (let cycle = 0; cycle < maxCycle; cycle++) {
+      const platformStringRepresentation = platform.toString();
+
+      const previousCycleStart = platformToCycleMap.get(platformStringRepresentation);
+      if (previousCycleStart !== undefined) {
+        const minCyclesNeeded =
+          previousCycleStart + ((maxCycle - previousCycleStart) % (cycle - previousCycleStart));
+        return cycleToTotalLoadMap.get(minCyclesNeeded)!;
+      }
+
+      platformToCycleMap.set(platformStringRepresentation, cycle);
+      cycleToTotalLoadMap.set(cycle, platform.getTotalLoad());
+      platform.cycle();
     }
 
-    // previous.set(stringRep, cycle);
-    let iterate = true;
-    // North
-    while (iterate) {
-      let didSlide = false;
-      for (let c = 0; c < spaces[0].length; c++) {
-        for (let r = 1; r < spaces.length; r++) {
-          if (spaces[r - 1][c] === '.' && spaces[r][c] === 'O') {
-            spaces[r - 1][c] = 'O';
-            spaces[r][c] = '.';
-            didSlide = true;
-          }
-        }
-      }
+    throw new Error('No cycle detected, try again nerd.');
+  };
 
-      if (!didSlide) {
-        iterate = false;
-      }
-    }
-
-    // West
-    //console.log('after north');
-    //console.log(spaces.map((s) => s.join('')).join('\n'));
-    iterate = true;
-    while (iterate) {
-      let didSlide = false;
-      for (let r = 0; r < spaces.length; r++) {
-        for (let c = 1; c < spaces[0].length; c++) {
-          if (spaces[r][c - 1] === '.' && spaces[r][c] === 'O') {
-            spaces[r][c - 1] = 'O';
-            spaces[r][c] = '.';
-            didSlide = true;
-          }
-        }
-      }
-
-      if (!didSlide) {
-        iterate = false;
-      }
-    }
-
-    // South
-    //console.log('after west');
-    //console.log(spaces.map((s) => s.join('')).join('\n'));
-    iterate = true;
-    while (iterate) {
-      let didSlide = false;
-      for (let c = 0; c < spaces[0].length; c++) {
-        for (let r = spaces.length - 2; r >= 0; r--) {
-          if (spaces[r + 1][c] === '.' && spaces[r][c] === 'O') {
-            spaces[r + 1][c] = 'O';
-            spaces[r][c] = '.';
-            didSlide = true;
-          }
-        }
-      }
-
-      if (!didSlide) {
-        iterate = false;
-      }
-    }
-
-    // East
-    //console.log('after south');
-    //console.log(spaces.map((s) => s.join('')).join('\n'));
-    iterate = true;
-    while (iterate) {
-      let didSlide = false;
-      for (let r = 0; r < spaces.length; r++) {
-        for (let c = spaces[0].length - 2; c >= 0; c--) {
-          if (spaces[r][c + 1] === '.' && spaces[r][c] === 'O') {
-            spaces[r][c + 1] = 'O';
-            spaces[r][c] = '.';
-            didSlide = true;
-          }
-        }
-      }
-
-      if (!didSlide) {
-        iterate = false;
-      }
-    }
-
-    //console.log('after east');
-    //console.log(spaces.map((s) => s.join('')).join('\n'));
-
-    let sum = 0;
-    for (let r = 0; r < lines.length; r++) {
-      sum += (lines.length - r) * spaces[r].filter((s) => s === 'O').length;
-    }
-
-    // if (sum === 64) {
-    //   console.log(cycle);
-    // }
-
-    if (!known.get(cycle)) {
-      known.set(cycle, []);
-    }
-
-    known.get(cycle)!.push(sum);
-  }
-
-  const loops = [...known].filter((x) => x[1].length > 1);
-  if (loops.length) {
-    debugger;
-  }
-
-  let sum = 0;
-  for (let r = 0; r < lines.length; r++) {
-    sum += (lines.length - r) * spaces[r].filter((s) => s === 'O').length;
-  }
-
-  return sum.toString();
+  return getTotalLoad().toString();
 };
