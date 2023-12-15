447aa1f5b1b05d74988daa6c6a8b32f788a67e0b
Andy <onsubmit@outlook.com>
2023-12-14 21:05:29 -0800

    Add day 15 part 1

--- src/days/day15.ts
+++ src/days/day15.ts
@@ -1,7 +1,19 @@
 export const getPartOneSolution = (input: string): string => {
-  const lines = input.split('\n').filter(Boolean);
+  const strings = input.trim().split(',');
 
-  return lines.toString();
+  let sum = 0;
+  for (const string of strings) {
+    let currentValue = 0;
+    for (const char of [...string]) {
+      const ascii = char.charCodeAt(0);
+      currentValue += ascii;
+      currentValue *= 17;
+      currentValue = currentValue % 256;
+    }
+    sum += currentValue;
+  }
+
+  return sum.toString();
 };
 
 export const getPartTwoSolution = (input: string): string => {
