4742666e0c02fba9a961be1d2fdc6a3d2fc3c0b6
Andy <onsubmit@outlook.com>
2023-12-16 11:13:47 -0800

    Clean up day 16

--- src/days/day16.ts
+++ src/days/day16.ts
@@ -105,11 +105,8 @@ const stepBeam = (space: Space, beam: Beam): Beam | void => {
           return beam.goSouth();
         case 'east': // E/W doesn't matter
         case 'west': {
-          // This beam goes North
           const newBeam = beam.clone();
           beam.goNorth();
-
-          // New beam goes South
           return newBeam.goSouth();
         }
       }
@@ -119,11 +116,8 @@ const stepBeam = (space: Space, beam: Beam): Beam | void => {
       switch (direction) {
         case 'north': // N/S doesn't matter
         case 'south': {
-          // This beam goes West
           const newBeam = beam.clone();
           beam.goWest();
-
-          // New beam goes East
           return newBeam.goEast();
         }
         case 'east':
@@ -136,10 +130,8 @@ const stepBeam = (space: Space, beam: Beam): Beam | void => {
   }
 };
 
-export const getPartOneSolution = (input: string): string => {
-  const lines = input.split('\n').filter(Boolean);
-
-  const spaces: Spaces = lines.map<Space[]>((line, row) =>
+const getInitialSpaces = (lines: string[]): Spaces =>
+  lines.map<Space[]>((line, row) =>
     [...line].map<Space>((value, column) => ({
       value: value as SpaceChar,
       coordinate: { row, column },
@@ -148,11 +140,8 @@ export const getPartOneSolution = (input: string): string => {
     }))
   );
 
-  let beamCounter = 0;
-  const beams: Map<number, Beam> = new Map([
-    [beamCounter, new Beam({ row: 0, column: 0 }, 'east')],
-  ]);
-
+const stepBeams = (spaces: Spaces, beams: Map<number, Beam>) => {
+  let beamCounter = beams.size;
   while (true) {
     let areBeamsStepping = false;
     for (const [beamNum, beam] of beams.entries()) {
@@ -173,10 +162,6 @@ export const getPartOneSolution = (input: string): string => {
         continue;
       }
 
-      console.log(
-        spaces.map((s) => s.map((x) => (x.isEnergized ? '#' : x.value)).join('')).join('\n')
-      );
-
       areBeamsStepping = true;
       space.isEnergized = true;
       space.previousBeamDirections.add(direction);
@@ -191,6 +176,14 @@ export const getPartOneSolution = (input: string): string => {
       break;
     }
   }
+};
+
+export const getPartOneSolution = (input: string): string => {
+  const lines = input.split('\n').filter(Boolean);
+  const spaces = getInitialSpaces(lines);
+
+  const beams: Map<number, Beam> = new Map([[0, new Beam({ row: 0, column: 0 }, 'east')]]);
+  stepBeams(spaces, beams);
 
   let count = 0;
   for (let r = 0; r < spaces.length; r++) {
@@ -205,262 +198,56 @@ export const getPartOneSolution = (input: string): string => {
 };
 
 export const getPartTwoSolution = (input: string): string => {
-  // const lines = input.split('\n').filter(Boolean);
+  const lines = input.split('\n').filter(Boolean);
 
-  // const loop = (beams: Map<number, Beam>) => {
-  //   while (true) {
-  //     let iterating = false;
-  //     for (const [beamNum, beam] of beams.entries()) {
-  //       const { coordinate, direction } = beam;
-  //       const { row, column } = coordinate;
-  //       if (row < 0 || row >= spaces.length || column < 0 || column >= spaces[0].length) {
-  //         // Beam fell of the edge, kill it.
-  //         beams.delete(beamNum);
-  //         continue;
-  //       }
+  let spaces = getInitialSpaces(lines);
 
-  //       if (spaces[row][column].previousBeamDirections.has(direction)) {
-  //         // A beam going this direction has already been in this space!
-  //         // No need to keep this beam around, the other beam is fine.
-  //         beams.delete(beamNum);
-  //         continue;
-  //       }
+  let max = Number.MIN_SAFE_INTEGER;
+  for (const row of [0, spaces.length - 1]) {
+    for (let column = 0; column < spaces[0].length; column++) {
+      spaces = getInitialSpaces(lines);
 
-  //       iterating = true;
-  //       spaces[row][column].isEnergized = true;
-  //       spaces[row][column].previousBeamDirections.add(direction);
-  //       switch (spaces[row][column].value) {
-  //         case '.': {
-  //           switch (direction) {
-  //             case 'north': {
-  //               position.row--;
-  //               break;
-  //             }
-  //             case 'south': {
-  //               position.row++;
-  //               break;
-  //             }
-  //             case 'east': {
-  //               position.column++;
-  //               break;
-  //             }
-  //             case 'west': {
-  //               position.column--;
-  //               break;
-  //             }
-  //           }
-  //           break;
-  //         }
-  //         case '/': {
-  //           switch (direction) {
-  //             case 'north': {
-  //               position.column++;
-  //               beam.direction = 'east';
-  //               break;
-  //             }
-  //             case 'south': {
-  //               position.column--;
-  //               beam.direction = 'west';
-  //               break;
-  //             }
-  //             case 'east': {
-  //               position.row--;
-  //               beam.direction = 'north';
-  //               break;
-  //             }
-  //             case 'west': {
-  //               position.row++;
-  //               beam.direction = 'south';
-  //               break;
-  //             }
-  //           }
-  //           break;
-  //         }
-  //         case '\\': {
-  //           switch (direction) {
-  //             case 'north': {
-  //               position.column--;
-  //               beam.direction = 'west';
-  //               break;
-  //             }
-  //             case 'south': {
-  //               position.column++;
-  //               beam.direction = 'east';
-  //               break;
-  //             }
-  //             case 'east': {
-  //               position.row++;
-  //               beam.direction = 'south';
-  //               break;
-  //             }
-  //             case 'west': {
-  //               position.row--;
-  //               beam.direction = 'north';
-  //               break;
-  //             }
-  //           }
-  //           break;
-  //         }
-  //         case '|': {
-  //           switch (direction) {
-  //             case 'north': {
-  //               position.row--;
-  //               break;
-  //             }
-  //             case 'south': {
-  //               position.row++;
-  //               break;
-  //             }
-  //             case 'east': {
-  //               // This beam goes North
-  //               const newBeam = beam.clone();
-  //               position.row--;
-  //               beam.direction = 'north';
+      const beams: Map<number, Beam> = new Map([
+        [0, new Beam({ row, column }, row === 0 ? 'south' : 'north')],
+      ]);
 
-  //               // New beam goes South
-  //               newBeam.position.row++;
-  //               newBeam.direction = 'south';
-  //               const newBeamNum = Math.max(...beams.keys()) + 1;
-  //               beams.set(newBeamNum, newBeam);
-  //               break;
-  //             }
-  //             case 'west': {
-  //               const newBeam = beam.clone();
-  //               // This beam goes North
-  //               position.row--;
-  //               beam.direction = 'north';
+      stepBeams(spaces, beams);
 
-  //               // New beam goes South
-  //               newBeam.position.row++;
-  //               newBeam.direction = 'south';
-  //               const newBeamNum = Math.max(...beams.keys()) + 1;
-  //               beams.set(newBeamNum, newBeam);
-  //               break;
-  //             }
-  //           }
-  //           break;
-  //         }
-  //         case '-': {
-  //           switch (direction) {
-  //             case 'north': {
-  //               // This beam goes West
-  //               const newBeam = structuredClone(beam);
-  //               position.column--;
-  //               beam.direction = 'west';
+      let count = 0;
+      for (let r = 0; r < spaces.length; r++) {
+        for (let c = 0; c < spaces[r].length; c++) {
+          if (spaces[r][c].isEnergized) {
+            count++;
+          }
+        }
+      }
 
-  //               // New beam goes East
-  //               newBeam.position.column++;
-  //               newBeam.direction = 'east';
-  //               const newBeamNum = Math.max(...beams.keys()) + 1;
-  //               beams.set(newBeamNum, newBeam);
-  //               break;
-  //             }
-  //             case 'south': {
-  //               // This beam goes West
-  //               const newBeam = structuredClone(beam);
-  //               position.column--;
-  //               beam.direction = 'west';
+      max = Math.max(max, count);
+    }
+  }
 
-  //               // New beam goes East
-  //               newBeam.position.column++;
-  //               newBeam.direction = 'east';
-  //               const newBeamNum = Math.max(...beams.keys()) + 1;
-  //               beams.set(newBeamNum, newBeam);
-  //               break;
-  //             }
-  //             case 'east': {
-  //               position.column++;
-  //               break;
-  //             }
-  //             case 'west': {
-  //               position.column--;
-  //               break;
-  //             }
-  //           }
-  //           break;
-  //         }
-  //       }
-  //     }
+  for (const column of [0, spaces[0].length - 1]) {
+    for (let row = 0; row < spaces.length; row++) {
+      spaces = getInitialSpaces(lines);
 
-  //     if (!iterating) {
-  //       break;
-  //     }
-  //   }
-  // };
+      const beams: Map<number, Beam> = new Map([
+        [0, new Beam({ row, column }, column === 0 ? 'east' : 'west')],
+      ]);
 
-  // let spaces: Space[][] = lines.map<Space[]>((line, row) =>
-  //   [...line].map<Space>((value, column) => ({
-  //     value: value as SpaceChar,
-  //     coordinate: { row, column },
-  //     isEnergized: false,
-  //     previousBeamDirections: new Set(),
-  //   }))
-  // );
+      stepBeams(spaces, beams);
 
-  // let max = Number.MIN_SAFE_INTEGER;
-  // for (const row of [0, spaces.length - 1]) {
-  //   for (let column = 0; column < spaces[0].length; column++) {
-  //     spaces = lines.map<Space[]>((line, row) =>
-  //       [...line].map<Space>((value, column) => ({
-  //         value: value as SpaceChar,
-  //         coordinate: { row, column },
-  //         isEnergized: false,
-  //         previousBeamDirections: new Set(),
-  //       }))
-  //     );
+      let count = 0;
+      for (let r = 0; r < spaces.length; r++) {
+        for (let c = 0; c < spaces[r].length; c++) {
+          if (spaces[r][c].isEnergized) {
+            count++;
+          }
+        }
+      }
 
-  //     const beams: Map<number, { position: Coordinate; direction: Direction }> = new Map([
-  //       [0, { position: { row, column }, direction: row === 0 ? 'south' : 'north' }],
-  //     ]);
+      max = Math.max(max, count);
+    }
+  }
 
-  //     loop(beams);
-
-  //     let count = 0;
-  //     for (let r = 0; r < spaces.length; r++) {
-  //       for (let c = 0; c < spaces[r].length; c++) {
-  //         if (spaces[r][c].isEnergized) {
-  //           count++;
-  //         }
-  //       }
-  //     }
-
-  //     max = Math.max(max, count);
-  //   }
-  // }
-
-  // for (const column of [0, spaces[0].length - 1]) {
-  //   for (let row = 0; row < spaces.length; row++) {
-  //     spaces = lines.map<Space[]>((line, row) =>
-  //       [...line].map<Space>((value, column) => ({
-  //         value: value as SpaceChar,
-  //         coordinate: { row, column },
-  //         isEnergized: false,
-  //         previousBeamDirections: new Set(),
-  //       }))
-  //     );
-
-  //     const beams: Map<number, { position: Coordinate; direction: Direction }> = new Map([
-  //       [0, { position: { row, column }, direction: column === 0 ? 'east' : 'west' }],
-  //     ]);
-
-  //     loop(beams);
-
-  //     let count = 0;
-  //     for (let r = 0; r < spaces.length; r++) {
-  //       for (let c = 0; c < spaces[r].length; c++) {
-  //         if (spaces[r][c].isEnergized) {
-  //           count++;
-  //         }
-  //       }
-  //     }
-
-  //     max = Math.max(max, count);
-  //   }
-  // }
-
-  // console.log(spaces.map((s) => s.map((x) => (x.isEnergized ? '#' : x.value)).join('')).join('\n'));
-
-  // return max.toString();
-
-  return '';
+  return max.toString();
 };
