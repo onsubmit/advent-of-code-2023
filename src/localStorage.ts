ed1aeee5ee029ab3256a878fa841d0f292486f79
Andy <onsubmit@outlook.com>
2023-12-16 20:57:02 -0800

    Sort cache keys

--- src/localStorage.ts
+++ src/localStorage.ts
@@ -3,15 +3,22 @@ type Cache = Record<string, string>;
 const LOCAL_STORAGE_KEY = 'cache';
 
 const getDayPartKey = (day: number, part: number): string => {
-  return [day, part].join();
+  return [day.toString().padStart(2, '0'), part].join();
 };
 
 export const setInput = (day: number, part: number, value: string): void => {
   const cache = getCache();
   cache[getDayPartKey(day, part)] = value;
 
+  const ordered = Object.keys(cache)
+    .sort()
+    .reduce<Cache>((obj, key) => {
+      obj[key] = cache[key];
+      return obj;
+    }, {});
+
   try {
-    const cacheStr = JSON.stringify(cache);
+    const cacheStr = JSON.stringify(ordered);
     localStorage.setItem(LOCAL_STORAGE_KEY, cacheStr);
   } catch (e) {
     console.error(e);
