1d3863e2e31a65942df701c2d5989812660c1979
Andy <onsubmit@outlook.com>
2023-12-18 11:26:10 -0800

    Add day 18 part 2

--- src/days/day18.ts
+++ src/days/day18.ts
@@ -99,7 +99,7 @@ export const getPartTwoSolution = (input: string): string => {
   const lines = input.split('\n').filter(Boolean);
 
   const plans: Plan[] = lines.map((line) => {
-    const [_, __, hex] = line.split(' ');
+    const hex = line.split(' ').at(-1)!;
     const hexArr = [...hex];
     const directions: Direction[] = ['R', 'D', 'L', 'U'];
     hexArr.pop(); // )
@@ -108,5 +108,37 @@ export const getPartTwoSolution = (input: string): string => {
     return { direction, distance, hex };
   });
 
-  return plans ? 'not implemented' : 'not implemented';
+  // Get all the corner coordinates.
+  const current: Coordinate = { row: 0, column: 0 };
+  const corners: Coordinate[] = [current];
+
+  const adjustCurrentFns: Record<Direction, (amount: number) => void> = {
+    R: (amount) => (current.column += amount),
+    L: (amount) => (current.column -= amount),
+    U: (amount) => (current.row -= amount),
+    D: (amount) => (current.row += amount),
+  };
+
+  let numBoundaryPoints = 0;
+  for (const plan of plans) {
+    numBoundaryPoints += plan.distance;
+    adjustCurrentFns[plan.direction](plan.distance);
+    corners.push({ ...current });
+  }
+
+  // https://en.wikipedia.org/wiki/Shoelace_formula
+  let area = 0;
+  for (let i = 0; i < corners.length - 1; i++) {
+    const cornerA = corners[i];
+    const cornerB = corners[i + 1];
+    area += (cornerA.row + cornerB.row) * (cornerA.column - cornerB.column);
+  }
+
+  area /= 2;
+
+  // https://en.wikipedia.org/wiki/Pick%27s_theorem
+  // A = i + b/2 - 1
+  // i = A - b/2 + 1
+  const interiorPoints = area - numBoundaryPoints / 2 + 1;
+  return (interiorPoints + numBoundaryPoints).toString();
 };
