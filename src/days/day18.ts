49f3a27ea5cbf9a08daf804ea0d3df4b4f9055f2
Andy <onsubmit@outlook.com>
2023-12-18 00:51:06 -0800

    Clean up day 18 part 1

--- src/days/day18.ts
+++ src/days/day18.ts
@@ -7,10 +7,38 @@ type Plan = {
   hex: string;
 };
 
+const getCoordinateKey = (c: Coordinate) => {
+  return [c.row, c.column].join(',');
+};
+
+const iterativeFloodFill = (row: number, column: number, arr: string[][]) => {
+  const queue: Coordinate[] = [{ row, column }];
+  while (queue.length) {
+    const { row: r, column: c } = queue.pop()!;
+    arr[r][c] = '#';
+
+    if (arr[r + 1]?.[c] === '.') {
+      queue.push({ row: r + 1, column: c });
+    }
+
+    if (arr[r - 1]?.[c] === '.') {
+      queue.push({ row: r - 1, column: c });
+    }
+
+    if (arr[r]?.[c + 1] === '.') {
+      queue.push({ row: r, column: c + 1 });
+    }
+
+    if (arr[r]?.[c - 1] === '.') {
+      queue.push({ row: r, column: c - 1 });
+    }
+  }
+};
+
 export const getPartOneSolution = (input: string): string => {
   const lines = input.split('\n').filter(Boolean);
 
-  const plans: Plan[] = lines.map((line) => {
+  const plans = lines.map<Plan>((line) => {
     const [dir, dist, hex] = line.split(' ');
     const direction = dir as Direction;
     const distance = parseInt(dist, 10);
@@ -20,141 +48,45 @@ export const getPartOneSolution = (input: string): string => {
   const trench: Map<string, string> = new Map();
   const current: Coordinate = { row: 0, column: 0 };
 
-  const coordinateKey = (c: Coordinate) => {
-    return [c.row, c.column].join(',');
-  };
-
-  trench.set(coordinateKey(current), '#');
-
-  for (const plan of plans) {
-    for (let i = 0; i < plan.distance; i++) {
-      switch (plan.direction) {
-        case 'R': {
-          current.column++;
-          break;
-        }
-        case 'L': {
-          current.column--;
-          break;
-        }
-        case 'U': {
-          current.row--;
-          break;
-        }
-        case 'D': {
-          current.row++;
-          break;
-        }
-      }
-      trench.set(coordinateKey(current), '#');
-    }
-  }
-
-  // Dig interior
-  const keys: Coordinate[] = [...trench.keys()].map((k) => {
-    const [r, c] = k.split(',');
-    const row = parseInt(r, 10);
-    const column = parseInt(c, 10);
-    return { row, column };
-  });
-
   let minRow = Number.MAX_SAFE_INTEGER;
   let minCol = Number.MAX_SAFE_INTEGER;
   let maxRow = Number.MIN_SAFE_INTEGER;
   let maxCol = Number.MIN_SAFE_INTEGER;
 
-  for (const key of keys) {
-    const { row, column } = key;
-    minRow = Math.min(minRow, row);
-    minCol = Math.min(minCol, column);
-    maxRow = Math.max(maxRow, row);
-    maxCol = Math.max(maxCol, column);
+  trench.set(getCoordinateKey(current), '#');
+  const adjustCurrentFns: Record<Direction, () => void> = {
+    R: () => current.column++,
+    L: () => current.column--,
+    U: () => current.row--,
+    D: () => current.row++,
+  };
+
+  for (const plan of plans) {
+    for (let i = 0; i < plan.distance; i++) {
+      adjustCurrentFns[plan.direction]();
+
+      minRow = Math.min(minRow, current.row);
+      minCol = Math.min(minCol, current.column);
+      maxRow = Math.max(maxRow, current.row);
+      maxCol = Math.max(maxCol, current.column);
+
+      trench.set(getCoordinateKey(current), '#');
+    }
   }
 
-  const arr: string[][] = Array.from({ length: Math.abs(minRow) + Math.abs(maxRow) + 1 }, (_, r) =>
-    Array.from({ length: Math.abs(minCol) + Math.abs(maxCol) + 1 }, (_, c) => {
-      const cr = r + minRow;
-      const cc = c + minCol;
-      return trench.has(coordinateKey({ row: cr, column: cc })) ? '#' : '.';
+  const trenchArray = Array.from({ length: maxRow - minRow + 1 }, (_, row) =>
+    Array.from({ length: maxCol - minCol + 1 }, (_, col) => {
+      return trench.has(getCoordinateKey({ row: row + minRow, column: col + minCol })) ? '#' : '.';
     })
   );
 
-  console.log(arr.map((l) => l.join('')).join('\n'));
-
-  //let count = 0;
-  //for (let r = 0; r < arr.length; r++) {
-  //   // For each row, mark the beginning and endpoints of each row of pounds
-  //   // The interior spaces will be between them.
-
-  //   const chunks: Array<[number, number]> = [];
-  //   for (let c = 0; c < arr[r].length; c++) {
-  //     let inChunk = false;
-  //     let start = -1;
-  //     let end = -1;
-  //     for (; c < arr[r].length; c++) {
-  //       if (arr[r][c] === '#') {
-  //         count++;
-  //         if (!inChunk) {
-  //           start = c;
-  //         }
-  //         if (c === arr[r].length - 1) {
-  //           end = c;
-  //         }
-  //         inChunk = true;
-  //       } else {
-  //         if (inChunk) {
-  //           end = c - 1;
-  //         }
-  //         break;
-  //       }
-  //     }
-
-  //     if (start >= 0 && end >= 0) {
-  //       chunks.push([start, end]);
-  //     }
-  //   }
-
-  //   for (let i = 1; i < chunks.length; i++) {
-  //     if (i % 2 === 1) {
-  //       count += chunks[i][0] - chunks[i - 1][1] - 1;
-  //     }
-  //   }
-  // }
-
-  //   let inside = false;
-  //   for (let c = 0; c < arr[r].length; c++) {
-  //     if (arr[r][c] === '#') {
-  //       let foundDot = false;
-  //       for (; c < arr[r].length; c++) {
-  //         if (arr[r][c] === '#') {
-  //           count++;
-  //         } else {
-  //           foundDot = true;
-  //           break;
-  //         }
-  //       }
-
-  //       if (foundDot) {
-  //         inside = !inside;
-  //       }
-  //     }
-
-  //     if (inside && arr[r][c] === '.') {
-  //       count++;
-  //     }
-  //   }
-  // }
-
-  //const arr2 = floodFill(arr, 75, 4, '#');
-
-  //console.log(arr2.map((l) => l.join('')).join('\n'));
-
-  floodFill(75, 4, arr);
+  // By input inspection, (75,4) is "interior".
+  iterativeFloodFill(75, 4, trenchArray);
 
   let count = 0;
-  for (let r = 0; r < arr.length; r++) {
-    for (let c = 0; c < arr[r].length; c++) {
-      if (arr[r][c] === '#') {
+  for (let r = 0; r < trenchArray.length; r++) {
+    for (let c = 0; c < trenchArray[r].length; c++) {
+      if (trenchArray[r][c] === '#') {
         count++;
       }
     }
@@ -163,107 +95,18 @@ export const getPartOneSolution = (input: string): string => {
   return count.toString();
 };
 
-const floodFill = (row: number, column: number, arr: string[][]) => {
-  //here check_validity is a function that given coordinates of the point tells you whether
-  //the point should be colored or not
-  const q: Coordinate[] = [];
-  q.push({ row, column });
-  while (q.length) {
-    const { row: r, column: c } = q.pop()!;
-    arr[r][c] = '#';
-
-    if (arr[r + 1]?.[c] === '.') {
-      q.push({ row: r + 1, column: c });
-    }
-
-    if (arr[r - 1]?.[c] === '.') {
-      q.push({ row: r - 1, column: c });
-    }
-
-    if (arr[r]?.[c + 1] === '.') {
-      q.push({ row: r, column: c + 1 });
-    }
-
-    if (arr[r]?.[c - 1] === '.') {
-      q.push({ row: r, column: c - 1 });
-    }
-  }
-
-  //  while (q is not empty)
-  //      (x1,y1) = q.pop()
-  //      color(x1,y1)
-
-  //      if (check_validity(x1+1,y1))
-  //           q.push(x1+1,y1)
-  //      if (check_validity(x1-1,y1))
-  //           q.push(x1-1,y1)
-  //      if (check_validity(x1,y1+1))
-  //           q.push(x1,y1+1)
-  //      if (check_validity(x1,y1-1))
-  //           q.push(x1,y1-1)
-};
-// const floodFill = (arr: string[][], row: number, column: number, newColor: string) => {
-//   //Get the input which needs to be replaced.
-//   const current = arr[row][column];
-
-//   //If the newColor is same as the existing
-//   //Then return the original image.
-//   if (current === newColor) {
-//     return arr;
-//   }
-
-//   //Other wise call the fill function which will fill in the existing image.
-//   fill(arr, row, column, newColor, current);
-
-//   //Return the image once it is filled
-//   return arr;
-// };
-
-// const fill = (arr: string[][], row: number, column: number, newColor: string, current: string) => {
-//   //If row is less than 0
-//   if (row < 0) {
-//     return;
-//   }
-
-//   //If column is less than 0
-//   if (column < 0) {
-//     return;
-//   }
-
-//   //If row is greater than image length
-//   if (row > arr.length - 1) {
-//     return;
-//   }
-
-//   //If column is greater than image length
-//   if (column > arr[row].length - 1) {
-//     return;
-//   }
-
-//   //If the current pixel is not which needs to be replaced
-//   if (arr[row][column] !== current) {
-//     return;
-//   }
-
-//   //Update the new color
-//   arr[row][column] = newColor;
-
-//   //Fill in all four directions
-//   //Fill Prev row
-//   fill(arr, row - 1, column, newColor, current);
-
-//   //Fill Next row
-//   fill(arr, row + 1, column, newColor, current);
-
-//   //Fill Prev col
-//   fill(arr, row, column - 1, newColor, current);
-
-//   //Fill next col
-//   fill(arr, row, column + 1, newColor, current);
-// };
-
 export const getPartTwoSolution = (input: string): string => {
   const lines = input.split('\n').filter(Boolean);
 
-  return lines.toString();
+  const plans: Plan[] = lines.map((line) => {
+    const [_, __, hex] = line.split(' ');
+    const hexArr = [...hex];
+    const directions: Direction[] = ['R', 'D', 'L', 'U'];
+    hexArr.pop(); // )
+    const direction = directions[parseInt(hexArr.pop()!, 10)];
+    const distance = parseInt(hexArr.slice(2).join(''), 16);
+    return { direction, distance, hex };
+  });
+
+  return plans ? 'not implemented' : 'not implemented';
 };
