17ac5c4f12fe30e661aa1a571ca976baaf9789b3
Andy <onsubmit@outlook.com>
2023-12-19 20:38:42 -0800

    Add day 20 boilerplate

---
+++ src/components/day20.tsx
@@ -0,0 +1,7 @@
+import { getPartOneSolution, getPartTwoSolution } from '../days/day20';
+import Day from './day';
+
+export default function Day20() {
+  const props = { day: 20, getPartOneSolution, getPartTwoSolution };
+  return <Day {...props} />;
+}
