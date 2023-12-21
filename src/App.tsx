5a46db2e201812c34e871d4bc377a31c983d52e7
Andy <onsubmit@outlook.com>
2023-12-20 20:08:31 -0800

    Add day 21 boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -52,6 +52,7 @@ export default function App() {
   const [value, setValue] = useState(getCurrentDayIndex() ?? 0);
 
   const DayComponents = [
+    lazy(() => import('./components/day21')),
     lazy(() => import('./components/day20')),
     lazy(() => import('./components/day19')),
     lazy(() => import('./components/day18')),
---
+++ src/components/day21.tsx
@@ -0,0 +1,7 @@
+import { getPartOneSolution, getPartTwoSolution } from '../days/day21';
+import Day from './day';
+
+export default function Day21() {
+  const props = { day: 21, getPartOneSolution, getPartTwoSolution };
+  return <Day {...props} />;
+}
